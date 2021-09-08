8da7c900e1124a60f4ea867d6fd6cf8bac991ae4
taichanNE30 <taichanNE30@users.noreply.github.com>
2021-09-08 17:28:20 +0000

    deploy: 2808b0d5d5c77ab3b2d573570181ef03dfbe76e9

---
+++ _nuxt/static/1631122091/article/pv57d9_4gk6s/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1631122091",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fpv57d9_4gk6s",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1631122091/article/test_article/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1631122091",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Ftest_article",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
