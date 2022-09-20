1d47130f910f49606676b1c509d49bce2affe460
taichanNE30 <taichanNE30@users.noreply.github.com>
2022-09-20 10:41:13 +0000

    deploy: 6409c5d6b88c2e6e5f61a8e4cd7007bc8acb8ef4

---
+++ _nuxt/static/1663670466/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002F","\u002Ftag\u002Fgadget","\u002Ftag\u002Ftech","\u002Ftag\u002Fnote","\u002Farticle\u002Ftest_article2","\u002Farticle\u002Fpv57d9_4gk6s","\u002Farticle\u002Ftest_article"]})
